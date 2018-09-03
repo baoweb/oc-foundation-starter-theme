e23b3b44812587e10b0dbf9853a33d73898d271d
Martin Kryl <martin@baoweb.cz>
2018-09-03 14:50:51 +0200

    First version of the theme boilerplate

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import foundation from 'foundation-sites';
+
+console.log('Sctipt started');
+
+$(document).foundation();
---
+++ templates/home.php
@@ -0,0 +1,17 @@
+<?php
+    $active = '';
+?>
+<html>
+<head>
+    <title>Homepage - template</title>
+    <?php include('inc/html_head.php'); ?>
+</head>
+<body>
+
+<?php include('inc/header.php'); ?>
+
+
+<?php include('inc/footer.php'); ?>
+
+</body>
+</html>
